7983fc6c42abe17c569b76ff224615de8d69e89f
PavelFomchenkov <donotwritemail@gmail.com>
2023-07-23 23:45:14 +0300

    Commit 4

--- src/main/java/com/flomaster/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/com/flomaster/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,5 +1,8 @@
 package com.flomaster.config;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -7,14 +10,21 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
     protected Class<?>[] getRootConfigClasses() {
         return null;
     }
-
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class[] {SpringConfig.class};
     }
-
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+            new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 }
